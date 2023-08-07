1a77837bc8006007ad15bcc4afd63ebb234143f1
2767mr <2767mr@gmx.at>
2023-08-07 17:57:48 +0200

    Fixed relative paths not being loaded via fetch

--- src/utils.ts
+++ src/utils.ts
@@ -26,5 +26,15 @@ export function fixedRandomInt(seed: string | number, min: number, max: number):
 
 
 export async function readJsonFromFile(path: string) {
+	if (path.startsWith('./assets/')) {
+		return fetch(path.slice(1)).then(resp => resp.json());
+	}
+	if (path.startsWith('/assets/')) {
+		return fetch(path).then(resp => resp.json());
+	}
+	if (path.startsWith('assets/')) {
+		return fetch('/' + path).then(resp => resp.json());
+	}
+
 	return JSON.parse((await fs.promises.readFile(path)) as unknown as string);
 }
